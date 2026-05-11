63b6c8da4872fc24efcd6f461a2cc5e0eaa34a67
tomonobu-watanabe-Pathos <tomonobu.watanabe@pathoslogos.co.jp>
2026-05-11 16:18:07 +0900

    HPbar

--- prerude-to-destruction/src/game/scenes/Game.ts
+++ prerude-to-destruction/src/game/scenes/Game.ts
@@ -16,6 +16,7 @@ export class Game extends Scene
         // 1. カードを作成 (x: 400, y: 300, 幅: 100, 高さ: 150)
     for(let i = 0; i < 10; i++){
         const card = this.add.rectangle(400, 300, 100, 150, 0xffffff);
+        card.setData('value', 10);
         
         // 縁取りを追加してカードっぽく
         card.setStrokeStyle(2, 0x000000);
@@ -44,26 +45,54 @@ export class Game extends Scene
             gameObject.setAlpha(1.0);
         });
     }
+    const HPbar = this.add.rectangle(400, 100, 100, 10, 0x00ff00);
+    HPbar.setOrigin(0, 0.5);
+    HPbar.setAlpha(1.0);
+    HPbar.setDepth(10);
+    HPbar.setData('value', 100);
+    HPbar.setData('max', 100);
+    HPbar.setData('min', 0);
+    HPbar.setStrokeStyle(2, 0x000000);
+    const HPbarText = this.add.text(HPbar.x, HPbar.y, HPbar.getData('value').toString(), { fontSize: '16px', color: '#000000', align: 'center' });
+    HPbarText.setOrigin(0, 0.5);
+    HPbarText.setDepth(11);
+    HPbarText.setAlpha(1.0);
 
+    const zone = this.add.zone(400, 500, 200, 250).setRectangleDropZone(200, 250);
 
+    const graphics = this.add.graphics();
+    graphics.lineStyle(2, 0xffff00);
+    graphics.strokeRect(zone.x - zone.input!.hitArea!.width / 2, zone.y - zone.input!.hitArea!.height / 2, zone.input!.hitArea!.width, zone.input!.hitArea!.height);
 
-        // this.camera = this.cameras.main;
-        // this.camera.setBackgroundColor(0x00ff00);
+    this.input.on('drop', (pointer: Phaser.Input.Pointer, gameObject: any, dropZone: Phaser.GameObjects.Zone) => {
+        gameObject.x = dropZone.x;
+        gameObject.y = dropZone.y;
+    });
 
-        // this.background = this.add.image(512, 384, 'background');
-        // this.background.setAlpha(0.5);
+    this.input.on('dragend', (pointer: Phaser.Input.Pointer, gameObject: any, dropped: boolean) => {
+        if (!dropped){
+            gameObject.x = gameObject.input.dragStartX;
+            gameObject.y = gameObject.input.dragStartY;
+        }
+    });
 
-        // this.msg_text = this.add.text(512, 384, 'Make something fun!\nand share it with us:\nsupport@phaser.io', {
-        //     fontFamily: 'Arial Black', fontSize: 38, color: '#ffffff',
-        //     stroke: '#000000', strokeThickness: 8,
-        //     align: 'center'
-        // });
-        // this.msg_text.setOrigin(0.5);
+    this.input.on('dragenter', (pointer: Phaser.Input.Pointer, gameObject: any, dropZone: Phaser.GameObjects.Zone) => {
+        graphics.clear();
+        graphics.lineStyle(2, 0x00ff00);
+        graphics.strokeRect(zone.x - zone.input!.hitArea!.width / 2, zone.y - zone.input!.hitArea!.height / 2, zone.input!.hitArea!.width, zone.input!.hitArea!.height);
+    });
 
-        // this.input.once('pointerdown', () => {
+    this.input.on('drop', (pointer: Phaser.Input.Pointer, gameObject: any, dropZone: Phaser.GameObjects.Zone) => {
+        HPbar.setData('value', HPbar.getData('value') - gameObject.getData('value'));
+        HPbarText.setText(HPbar.getData('value').toString());
 
-        //     this.scene.start('GameOver');
+        const ratio = HPbar.getData('value') / HPbar.getData('max');
+        HPbar.setScale(ratio, 1.0);
 
-        // });
+        if (HPbar.getData('value') <= 0){
+            this.scene.start('GameOver');
+        }
+    });
+
     }
 }
