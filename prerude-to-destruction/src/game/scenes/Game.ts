3f346c9c0e55dddf330646e43859d8720808694f
tomonobu-watanabe-Pathos <tomonobu.watanabe@pathoslogos.co.jp>
2026-05-12 15:49:54 +0900

    player&cpu HP

--- prerude-to-destruction/src/game/scenes/Game.ts
+++ prerude-to-destruction/src/game/scenes/Game.ts
@@ -18,13 +18,14 @@ export class Game extends Scene
     }
 
     private deck: CardData[] = [];
+    private trash: CardData[] = [];
 
     create ()
     {
 
     this.initializeDeck();
 
-    const deckVisual = this.add.rectangle(700, 500, 100, 150, 0x555555);
+    const deckVisual = this.add.rectangle(500, 400, 100, 150, 0x555555);
     deckVisual.setInteractive();
 
     for(let i=0; i<5; i++){
@@ -121,27 +122,86 @@ export class Game extends Scene
     HPbarText.setOrigin(0, 0.5);
     HPbarText.setDepth(11);
     HPbarText.setAlpha(1.0);
+    
+    const playerHPbar = this.add.rectangle(900, 400, 100, 10, 0x00ff00);
+    playerHPbar.setOrigin(0, 0.5);
+    playerHPbar.setAlpha(1.0);
+    playerHPbar.setDepth(10);
+    playerHPbar.setData('value', 100);
+    playerHPbar.setData('max', 100);
+    playerHPbar.setData('min', 0);
+    playerHPbar.setStrokeStyle(2, 0x000000);
+    const playerHPbarText = this.add.text(playerHPbar.x, playerHPbar.y, playerHPbar.getData('value').toString(), { fontSize: '16px', color: '#000000', align: 'center' });
+    playerHPbarText.setOrigin(0, 0.5);
+    playerHPbarText.setDepth(11);
+    playerHPbarText.setAlpha(1.0);
 
-    const zone = this.add.zone(400, 200, 100, 150).setRectangleDropZone(100, 150);
+    const cpuHPbar = this.add.rectangle(100, 300, 100, 10, 0x00ff00);
+    cpuHPbar.setOrigin(0, 0.5);
+    cpuHPbar.setAlpha(1.0);
+    cpuHPbar.setDepth(10);
+    cpuHPbar.setData('value', 100);
+    cpuHPbar.setData('max', 100);
+    cpuHPbar.setData('min', 0);
+    cpuHPbar.setStrokeStyle(2, 0x000000);
+    const cpuHPbarText = this.add.text(cpuHPbar.x, cpuHPbar.y, cpuHPbar.getData('value').toString(), { fontSize: '16px', color: '#000000', align: 'center' });
+    cpuHPbarText.setOrigin(0, 0.5);
+    cpuHPbarText.setDepth(11);
+    cpuHPbarText.setAlpha(1.0);
+
+    // Zone作成
+    // const zone = this.add.zone(400, 200, 100, 150).setRectangleDropZone(100, 150);
+
+    const cpuTargetZone = this.add.zone(100, 200, 100, 150).setRectangleDropZone(100, 150);
+    const playerTargetZone = this.add.zone(900, 550, 100, 150).setRectangleDropZone(100, 150);
 
     const graphics = this.add.graphics();
     graphics.lineStyle(2, 0xffff00);
-    graphics.strokeRect(zone.x - zone.input!.hitArea!.width / 2, zone.y - zone.input!.hitArea!.height / 2, zone.input!.hitArea!.width, zone.input!.hitArea!.height);
+    graphics.strokeRect(cpuTargetZone.x - cpuTargetZone.input!.hitArea!.width / 2, cpuTargetZone.y - cpuTargetZone.input!.hitArea!.height / 2, cpuTargetZone.input!.hitArea!.width, cpuTargetZone.input!.hitArea!.height);
+    graphics.strokeRect(playerTargetZone.x - playerTargetZone.input!.hitArea!.width / 2, playerTargetZone.y - playerTargetZone.input!.hitArea!.height / 2, playerTargetZone.input!.hitArea!.width, playerTargetZone.input!.hitArea!.height);
 
     // Zoneにドラッグしてきたときの処理
     this.input.on('dragenter', (pointer: Phaser.Input.Pointer, gameObject: any, dropZone: Phaser.GameObjects.Zone) => {
         graphics.clear();
         graphics.lineStyle(2, 0x00ff00);
-        graphics.strokeRect(zone.x - zone.input!.hitArea!.width / 2, zone.y - zone.input!.hitArea!.height / 2, zone.input!.hitArea!.width, zone.input!.hitArea!.height);
+        graphics.strokeRect(cpuTargetZone.x - cpuTargetZone.input!.hitArea!.width / 2, cpuTargetZone.y - cpuTargetZone.input!.hitArea!.height / 2, cpuTargetZone.input!.hitArea!.width, cpuTargetZone.input!.hitArea!.height);
+        graphics.strokeRect(playerTargetZone.x - playerTargetZone.input!.hitArea!.width / 2, playerTargetZone.y - playerTargetZone.input!.hitArea!.height / 2, playerTargetZone.input!.hitArea!.width, playerTargetZone.input!.hitArea!.height);
     });
 
     this.input.on('dragleave', (pointer: Phaser.Input.Pointer, gameObject: any, dropZone: Phaser.GameObjects.Zone) => {
         graphics.clear();
         graphics.lineStyle(2, 0xffff00);
-        graphics.strokeRect(zone.x - zone.input!.hitArea.width / 2, zone.y - zone.input!.hitArea.height / 2, zone.input!.hitArea.width, zone.input!.hitArea.height);
+        graphics.strokeRect(cpuTargetZone.x - cpuTargetZone.input!.hitArea!.width / 2, cpuTargetZone.y - cpuTargetZone.input!.hitArea!.height / 2, cpuTargetZone.input!.hitArea!.width, cpuTargetZone.input!.hitArea!.height);
+        graphics.strokeRect(playerTargetZone.x - playerTargetZone.input!.hitArea!.width / 2, playerTargetZone.y - playerTargetZone.input!.hitArea!.height / 2, playerTargetZone.input!.hitArea!.width, playerTargetZone.input!.hitArea!.height);
     });
 
     let depth = 1;
+
+    // Zoneにドロップしたときの効果処理
+    // this.input.on('drop', (pointer: Phaser.Input.Pointer, gameObject: Phaser.GameObjects.Container, dropZone: Phaser.GameObjects.Zone) => {
+    //     const container = gameObject.parentContainer;
+    //     container.x = dropZone.x;
+    //     container.y = dropZone.y;
+    //     container.setAngle(0);
+    //     container.setDepth(depth);
+    //     depth++;
+
+    //     const index = this.handCards.indexOf(container);
+    //     if (index > -1){
+    //         this.handCards.splice(index, 1);
+    //     }
+        
+    //     HPbar.setData('value', HPbar.getData('value') - gameObject.getData('value'));
+    //     HPbarText.setText(HPbar.getData('value').toString());
+
+    //     const ratio = HPbar.getData('value') / HPbar.getData('max');
+    //     HPbar.setScale(ratio, 1.0);
+
+    //     if (HPbar.getData('value') <= 0){
+    //         this.scene.start('GameOver');
+    //     }
+    // });
+    // }
 
     // Zoneにドロップしたときの効果処理
     this.input.on('drop', (pointer: Phaser.Input.Pointer, gameObject: Phaser.GameObjects.Container, dropZone: Phaser.GameObjects.Zone) => {
@@ -156,22 +216,46 @@ export class Game extends Scene
         if (index > -1){
             this.handCards.splice(index, 1);
         }
-        
-        HPbar.setData('value', HPbar.getData('value') - gameObject.getData('value'));
-        HPbarText.setText(HPbar.getData('value').toString());
 
-        const ratio = HPbar.getData('value') / HPbar.getData('max');
-        HPbar.setScale(ratio, 1.0);
+        if(dropZone === cpuTargetZone){
+            this.cpuHP.current -= gameObject.getData('value');
+            cpuHPbar.setData('value', this.cpuHP.current);
+            cpuHPbarText.setText(this.cpuHP.current.toString());
+            const ratio = this.cpuHP.current / this.cpuHP.max;
+            cpuHPbar.setScale(ratio, 1.0);
+            if (this.cpuHP.current <= 0){
+                this.scene.start('GameOver');
+            }
+        } else if(dropZone === playerTargetZone){
+            this.playerHP.current -= gameObject.getData('value');
+            playerHPbar.setData('value', this.playerHP.current);
+            playerHPbarText.setText(this.playerHP.current.toString());
+            const ratio = this.playerHP.current / this.playerHP.max;
+            playerHPbar.setScale(ratio, 1.0);
+            if (this.playerHP.current <= 0){
+                this.scene.start('GameOver');
+            }
+        // HPbar.setData('value', HPbar.getData('value') - gameObject.getData('value'));
+        // HPbarText.setText(HPbar.getData('value').toString());
 
-        if (HPbar.getData('value') <= 0){
-            this.scene.start('GameOver');
+        // const ratio = HPbar.getData('value') / HPbar.getData('max');
+        // HPbar.setScale(ratio, 1.0);
+
+        // if (HPbar.getData('value') <= 0){
+        //     this.scene.start('GameOver');
+        // }
         }
     });
-
     }
 
     // 手札のカードを管理する配列
     private handCards: Phaser.GameObjects.Container[] = [];
+    private playerHandCards: Phaser.GameObjects.Container[] = [];
+    private cpuHandCards: Phaser.GameObjects.Container[] = [];
+
+    // プレイヤーとCPUのHPを管理するオブジェクト
+    private playerHP = {current: 100, max: 100, text: null, bar: null };
+    private cpuHP = {current: 100, max: 100, text: null, bar: null };
 
     // 手札のレイアウトを更新するメソッド
     updateHandLayout(){
@@ -240,10 +324,8 @@ export class Game extends Scene
         this.input.setDraggable(card);
 
         if(this.deck.length === 0){
-            const emptyDeck = this.add.rectangle(700, 500, 100, 150, 0xeeeeee);
+            const emptyDeck = this.add.rectangle(500, 400, 100, 150, 0xeeeeee);
         }
         return container;
-
-
     }
 }
